64344d3f87146f25f3010ddbb11782af65ff4b79
gechoto <124326167+gechoto@users.noreply.github.com>
2025-01-11 02:36:09 +0000

    app: MusicService: use existing playbackData to recover song if possible

--- app/src/main/java/com/dd3boh/outertune/playback/MusicService.kt
+++ app/src/main/java/com/dd3boh/outertune/playback/MusicService.kt
@@ -449,6 +449,7 @@ class MusicService : MediaLibraryService(),
 
     private suspend fun recoverSong(mediaId: String, playbackData: YTPlayerUtils.PlaybackData? = null) {
         val playbackUrl = database.format(mediaId).first()?.playbackUrl
+            ?: playbackData?.playbackTracking?.videostatsPlaybackUrl?.baseUrl
             ?: YTPlayerUtils.playerResponseForMetadata(mediaId).getOrNull()?.playbackTracking?.videostatsPlaybackUrl?.baseUrl
 
         playbackUrl?.let {
