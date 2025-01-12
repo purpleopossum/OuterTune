29b10d7f5ed1d1b3d1d684692282898abc8c4c3a
mikooomich <mikooomichael@gmail.com>
2025-01-11 19:33:44 -0500

    gradle: Version bump to 0.7.0-rc1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,8 +19,8 @@ android {
         applicationId = "com.dd3boh.outertune"
         minSdk = 24
         targetSdk = 35
-        versionCode = 32
-        versionName = "0.7.0-beta2"
+        versionCode = 33
+        versionName = "0.7.0-rc1"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
     buildTypes {
