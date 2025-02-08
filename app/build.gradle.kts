51f4c4dfe6b33e332f12b1fa54b59da9775dffa2
mikooomich <mikooomichael@gmail.com>
2025-02-08 18:04:42 -0500

    gradle: Version bump to 0.7.3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,8 +17,8 @@ android {
         applicationId = "com.dd3boh.outertune"
         minSdk = 24
         targetSdk = 35
-        versionCode = 36
-        versionName = "0.7.2"
+        versionCode = 37
+        versionName = "0.7.3"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
     buildTypes {
